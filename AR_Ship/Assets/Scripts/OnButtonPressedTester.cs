3ed3fa6544ba5c14995993fba73d2e98df66de17
Amiin Najjar <najjar.amiin@gmail.com>
2024-07-11 11:14:16 +0200

    init

---
+++ AR_Ship/Assets/Scripts/OnButtonPressedTester.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class OnButtonPressedTester : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        UnityEngine.Debug.Log("Button was pressed!");
+    }
+}
