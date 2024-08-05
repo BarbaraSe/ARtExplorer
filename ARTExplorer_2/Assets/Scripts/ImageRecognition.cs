785a05d1dd48bd49b352111510f52b7775b34230
LukPle <lukasplenk@yahoo.de>
2024-08-05 16:04:15 +0200

    added reference images

---
+++ ARTExplorer_2/Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindAnyObjectByType<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
