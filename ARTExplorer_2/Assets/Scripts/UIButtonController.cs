674ac82932c7f7843a58ac642157a9b3d657cd9a
Barbara Se <seidinger-barbara@gmx.de>
2024-08-06 13:56:18 +0200

    added ui panel and solverhandler

---
+++ ARTExplorer_2/Assets/Scripts/UIButtonController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonController : MonoBehaviour
+{
+    public void InfoBtnPressed(){
+
+    }
+
+    // removes 3D objects shows only painting
+    public void TwoDBtnPressed(){
+
+    }
+
+    // shows 3D objects
+    public void ThreeDBtnPressed(){
+
+    }
+  
+    public void UndoPositionBtnPressed(){
+
+    }
+
+    public void UndoRotationBtnPressed(){
+        
+    }
+    
+
+    public void UndoScaleBtnPressed(){
+            
+    }
+
+    public void UndoObjectAllBtnPressed(){
+            
+    }
+
+    public void UndoAllBtnPressed(){
+            
+    }
+}
