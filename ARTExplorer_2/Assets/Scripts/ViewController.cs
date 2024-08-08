498d8b991e825031c71ac0727fcd95ba195a5408
Barbara Se <seidinger-barbara@gmx.de>
2024-08-08 17:00:49 +0200

    finished introduction screens, added images

--- ARTExplorer_2/Assets/Scripts/ViewController.cs
+++ ARTExplorer_2/Assets/Scripts/ViewController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ViewController : MonoBehaviour
 {
@@ -12,12 +13,15 @@ public class ViewController : MonoBehaviour
     private GameObject imageTargetHarbor;
     [SerializeField]
     private GameObject imageTargetDinner;
+    [SerializeField]
+    private TMP_Text swipeText;
+    private float delay = 2f;
+    
     public GameObject WelcomeScreen {set; get;}
     public GameObject IntroductionScreen{set; get;}
     public GameObject ImageTargetHarbor{set; get;}
     public GameObject ImageTargetDinner{set; get;}
 
-    
     void Start()
     {    
         WelcomeScreen = welcomeScreen;
@@ -25,14 +29,18 @@ public class ViewController : MonoBehaviour
         ImageTargetHarbor = imageTargetHarbor;
         ImageTargetDinner = imageTargetDinner;
 
+        swipeText.gameObject.SetActive(false);
         welcomeScreen.SetActive(true);
         introductionScreen.SetActive(false);
         imageTargetDinner.SetActive(false);
         imageTargetHarbor.SetActive(false);
+
+        StartCoroutine(EnableTextCoroutine());
     }
 
-    void Update()
+    private IEnumerator EnableTextCoroutine()
     {
-        
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        swipeText.gameObject.SetActive(true); // Enable the text
     }
 }
