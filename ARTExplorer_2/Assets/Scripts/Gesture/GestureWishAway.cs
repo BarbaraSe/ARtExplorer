76ec20f07a62f2783fa6796c8867626f614e58b5
Barbara Se <seidinger-barbara@gmx.de>
2024-08-08 16:27:04 +0200

    fixed wish away gesture

--- ARTExplorer_2/Assets/Scripts/Gesture/GestureWishAway.cs
+++ ARTExplorer_2/Assets/Scripts/Gesture/GestureWishAway.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class GestureWishAway : MonoBehaviour
 {
-    ViewController viewController;
-    private float gestureDistance = 0.1f;  
+    public ViewController viewController;
+    private float gestureDistance = 0.1f;
     private float gestureTime = 0.5f;
-    
-    private Vector3 startPosition;
+
+    private Vector3 startPositionL;
+    private Vector3 startPositionR;
     private float gestureStartTime;
     private bool gestureInProgress = false;
 
+    void Start(){
+        viewController = FindObjectOfType<ViewController>();
+    }
+
     void Update()
     {
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out MixedRealityPose palmPose))
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out MixedRealityPose palmPoseL))
         {
-            Vector3 currentPosition = palmPose.Position;
+            Vector3 currentPositionL = palmPoseL.Position;
 
             if (!gestureInProgress)
             {
-                startPosition = currentPosition;
+                startPositionL = currentPositionL;
                 gestureStartTime = Time.time;
                 gestureInProgress = true;
             }
             else
             {
-                if (Vector3.Distance(startPosition, currentPosition) >= gestureDistance)
+                if (Vector3.Distance(startPositionL, currentPositionL) >= gestureDistance)
+                {
+                    if (Time.time - gestureStartTime <= gestureTime)
+                    {
+                        OpenCloseMenu();
+                    }
+                    gestureInProgress = false;
+                }
+                else if (Time.time - gestureStartTime > gestureTime)
+                {
+                    gestureInProgress = false;
+                }
+            }
+        }
+        else if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Right, out MixedRealityPose palmPoseRight))
+        {
+            Vector3 currentPositionR = palmPoseRight.Position;
+
+            if (!gestureInProgress)
+            {
+                startPositionR = currentPositionR;
+                gestureStartTime = Time.time;
+                gestureInProgress = true;
+            }
+            else
+            {
+                if (Vector3.Distance(startPositionR, currentPositionR) >= gestureDistance)
                 {
                     if (Time.time - gestureStartTime <= gestureTime)
                     {
@@ -48,10 +79,13 @@ public class GestureWishAway : MonoBehaviour
 
     void OpenCloseMenu()
     {
-        if (viewController.WelcomeScreen.activeSelf) {
+        if (viewController.WelcomeScreen.activeSelf)
+        {
             viewController.IntroductionScreen.SetActive(true);
             viewController.WelcomeScreen.SetActive(false);
-        } else if (viewController.IntroductionScreen.activeSelf) {
+        }
+        else if (viewController.IntroductionScreen.activeSelf)
+        {
             viewController.IntroductionScreen.SetActive(false);
 
             // start image recognition
