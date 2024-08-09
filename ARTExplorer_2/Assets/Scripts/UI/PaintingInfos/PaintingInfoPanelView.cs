f1d7902dc7ba6aa2123f493da62a28581f4e69a2
Barbara Se <seidinger-barbara@gmx.de>
2024-08-09 19:01:38 +0200

    added all infos

--- ARTExplorer_2/Assets/Scripts/UI/PaintingInfos/PaintingInfoPanelView.cs
+++ ARTExplorer_2/Assets/Scripts/UI/PaintingInfos/PaintingInfoPanelView.cs
@@ -30,6 +30,14 @@ public class PaintingInfoPanelView : MonoBehaviour
     private TMP_Text _detail4;
 
     private PaintingInfoScreensController _paintingInfoScreensController;
+    private TMP_Text[] titles;
+    private TMP_Text[] details;
+
+
+    private void Start() {
+        titles = new TMP_Text[4]{_title1, _title2, _title3, _title4};
+        titles = new TMP_Text[4]{_detail1, _detail2, _detail3, _detail4};
+    }
 
     void Update() {
         _paintingInfoScreensController = FindObjectOfType<PaintingInfoScreensController>();
@@ -37,16 +45,31 @@ public class PaintingInfoPanelView : MonoBehaviour
 
     public void SetTextAboutSignificance(string str)
     {
+       _title1.text = "Significance";
+       _detail1.text = str;
+       for (int i = 1; i < titles.Length; i++)
+       {
+            titles[i].enabled = false;
+            details[i].enabled  = false;
+            _infoPanelTitle.enabled = false;
+       }
+    }
 
+    public void SetInfoPanelHeader(string text){
+        _infoPanelTitle.text = text;
     }
 
     public void SetText(List<DetailItem> list)
     {
-       _panel.SetActive(true);
+       int idx = 0;
        foreach (var item in list)
         {
-
+            if (idx <4) {
+                titles[idx].text = item.Title;
+                details[idx].text = item.Detail;
+                idx += 1;
+            }
+            
         }
     }
-
 }
--- ARTExplorer_2/Assets/Scripts/UI/PaintingInfos/PaintingInfoStartScreenView.cs
+++ ARTExplorer_2/Assets/Scripts/UI/PaintingInfos/PaintingInfoStartScreenView.cs
@@ -34,24 +34,54 @@ public class PaintingInfoStartScreenView : MonoBehaviour
    }
 
    public void ShowArtistInfo(){
-      
+      if (_paintingInfoScreensController._paintingInfos[index].AboutArtist != null) {
+         _paintingInfoScreensController.SetStartInfoPanelActive(false);
+         _paintingInfoScreensController.SetPaintingInfoActive(true);
+         List<DetailItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].AboutArtist;
+         _paintingInfoPanelView.SetText(artistInfo);
+         _paintingInfoPanelView.SetInfoPanelHeader("About Artist");
+      }
    }
 
    public void ShowPaintingInfo(){
-    _paintingInfoScreensController.SetAboutInfoActive(true);
-    List<AboutPaintingItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].AboutPainting;
-    _paintingInfoAboutPaintingView.SetTextAboutPainting(artistInfo);
+      _paintingInfoScreensController.SetStartInfoPanelActive(false);
+      _paintingInfoScreensController.SetAboutInfoActive(true);
+      List<AboutPaintingItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].AboutPainting;
+      _paintingInfoAboutPaintingView.SetTextAboutPainting(artistInfo);
    }
    public void ShowEraInfo(){
-    
+       if (_paintingInfoScreensController._paintingInfos[index].AboutEra != null) {
+         _paintingInfoScreensController.SetStartInfoPanelActive(false);
+         _paintingInfoScreensController.SetPaintingInfoActive(true);
+         List<DetailItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].AboutEra;
+         _paintingInfoPanelView.SetText(artistInfo);
+         _paintingInfoPanelView.SetInfoPanelHeader("About Era");
+       }
    }
    public void ShowSignificanceInfo(){
-    
+      if (_paintingInfoScreensController._paintingInfos[index].Significance != null) {
+         _paintingInfoScreensController.SetStartInfoPanelActive(false);
+         _paintingInfoScreensController.SetPaintingInfoActive(true);
+         string artistInfo = _paintingInfoScreensController._paintingInfos[index].Significance;
+         _paintingInfoPanelView.SetTextAboutSignificance(artistInfo);
+      }
    }
    public void ShowFunFactsInfo(){
-    
+      if (_paintingInfoScreensController._paintingInfos[index].FunFacts != null) {
+         _paintingInfoScreensController.SetStartInfoPanelActive(false);
+         _paintingInfoScreensController.SetPaintingInfoActive(true);
+         List<DetailItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].FunFacts;
+         _paintingInfoPanelView.SetText(artistInfo);
+         _paintingInfoPanelView.SetInfoPanelHeader("Fun Facts");
+      }
    }
    public void ShowKeyPointsInfo(){
-    
+      if (_paintingInfoScreensController._paintingInfos[index].KeyPointsEra != null) {
+         _paintingInfoScreensController.SetStartInfoPanelActive(false);
+         _paintingInfoScreensController.SetPaintingInfoActive(true);
+         List<DetailItem> artistInfo = _paintingInfoScreensController._paintingInfos[index].KeyPointsEra;
+         _paintingInfoPanelView.SetText(artistInfo);
+         _paintingInfoPanelView.SetInfoPanelHeader("Key Points about Era");
+      }
    }
 }
